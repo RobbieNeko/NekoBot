fe32aeb734a7d834db27edbd8ecda561d7030ca7
RobbieNeko <30732426+RobbieNeko@users.noreply.github.com>
2025-09-14 00:02:57 -0700

    Port `about` and `avatar` commands
    Simple and an easy starting place to make sure that I know what I'm doing.

--- nekobot_main.py
+++ nekobot_main.py
@@ -11,6 +11,22 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="$", intents=intents)
 
 @bot.tree.command(guild=MY_GUILD)
+async def about(interaction: discord.Interaction):
+    """Prints basic 'about' info"""
+    info = discord.Embed(title='About Nekobot')
+    info.add_field(name="Developer(s)", value="RosaAeterna (aka NekoRobbie)")
+    info.add_field(name="Library", value="Discord.py")
+    info.add_field(name="License", value="GNU GPL v3")
+    await interaction.response.send_message(embed=info)
+
+@bot.tree.command(guild=MY_GUILD)
+async def avatar(interaction: discord.Interaction):
+    """Shows your avatar"""
+    disp = discord.Embed(description=f"{interaction.user.display_name}'s Avatar\n[Full Image]({interaction.user.display_avatar.url})")
+    disp.set_thumbnail(url=interaction.user.display_avatar.url)
+    await interaction.response.send_message(embed=disp)
+
+@bot.tree.command(guild=MY_GUILD)
 async def testimage(interaction:discord.Interaction):
     """Posts a test image"""
     await interaction.response.send_message("https://img4.gelbooru.com/images/31/2f/312fc11a21c5d4ce06dc3aa8bfbb7221.jpg")
