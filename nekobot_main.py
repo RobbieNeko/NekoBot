50453e2af8574c64a9716c38482e06adf80f0c45
RobbieNeko <30732426+RobbieNeko@users.noreply.github.com>
2025-09-14 20:50:47 -0700

    Add baka command
    Tags could probably do with some additional refinement / a 'standard negatives' implemented, but overall works well enough

--- nekobot_main.py
+++ nekobot_main.py
@@ -1,6 +1,7 @@
 import discord
-import aiohttp
 from discord.ext import commands
+
+from helper_funcs import *
 
 # FIXME Remove for final deployment, this is just for testing purposes
 MY_GUILD = discord.Object(id='INSERT YOUR ID HERE (as an integer)')
@@ -25,6 +26,25 @@ async def avatar(interaction: discord.Interaction):
     disp = discord.Embed(description=f"{interaction.user.display_name}'s Avatar\n[Full Image]({interaction.user.display_avatar.url})")
     disp.set_thumbnail(url=interaction.user.display_avatar.url)
     await interaction.response.send_message(embed=disp)
+
+@bot.tree.command(guild=MY_GUILD)
+@discord.app_commands.describe(target="User you want to target (optional)" )
+async def baka(interaction: discord.Interaction, target: discord.User|None = None):
+    """Calls the target a baka!"""
+    if target == None: #Funny little response like the original
+        await interaction.response.send_message("Who are you trying to call a baka...?")
+    else:
+        if target == bot.user:
+            link = await safebooru_image("1girl+crying+solo+sad")
+            img = await file_from_url(link, "crying_baka.png")
+            await interaction.response.send_message("I-I'm not a baka, Y-YOU'RE A BAKA!! ;-;", file=img)
+        elif target == interaction.user:
+            img = discord.File("./resources/images/selfbaka.jpg")
+            await interaction.response.send_message("You're such a baka you just called yourself a baka!", file=img)
+        else:
+            link = await safebooru_image("pointing_at_another+(+(+1girl+1boy+)+~+2girls+~+2boys+)")
+            img = await file_from_url(link, "baka.png")
+            await interaction.response.send_message(f"{interaction.user.mention} just called {target.mention} a baka!", file=img)
 
 @bot.tree.command(guild=MY_GUILD)
 async def testimage(interaction:discord.Interaction):
